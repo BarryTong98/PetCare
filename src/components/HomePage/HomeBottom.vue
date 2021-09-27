14888d34a2c2569825f3ce5c8ccb0c9c60ceeeb2
barrytong <371202041@qq.com>
2021-09-27 16:18:56 +0800

    more component

---
+++ src/components/HomePage/HomeBottom.vue
@@ -0,0 +1,13 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  name: 'HomeBottom'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HomePage/Welcome.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <el-carousel :interval="4000" type="card">
+        <el-carousel-item v-for="item in itemList" :key="item">
+          <img :src="item" alt="missing"/>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      itemList: [
+        require('../../assets/img/cristian-castillo-73pyV0JJOmE-unsplash.jpeg'),
+        require('../../assets/img/martha-dominguez-de-gouveia-nMyM7fxpokE-unsplash.jpeg'),
+        require('../../assets/img/autri-taheri-l0Gq4BmboYY-unsplash.jpeg'),
+        require('../../assets/img/markus-winkler-PcKhVNNyEio-unsplash.jpeg')
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+div{
+  font-family: Arial,Helvetica,sans-serif;
+}
+</style>
