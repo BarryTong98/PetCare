d4e1be72b27b2773f967b068ece1836e20ea5502
king <947079912@qq.com>
2021-10-08 15:13:55 +0800

    change type of service price to String

--- petCare-web/src/main/java/au/edu/sydney/web/entity/pojo/Service.java
+++ petCare-web/src/main/java/au/edu/sydney/web/entity/pojo/Service.java
@@ -2,11 +2,12 @@ package au.edu.sydney.web.entity.pojo;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import java.util.Date;
 
-
-@ApiModel(value="Service", description="服务对象")
+@Data
+@ApiModel(value = "Service", description = "服务对象")
 public class Service {
 
     @ApiModelProperty(value = "服务ID")
@@ -19,7 +20,7 @@ public class Service {
     private String serviceName;
 
     @ApiModelProperty(value = "（预期）价格")
-    private Double price;
+    private String price;
 
     @ApiModelProperty(value = "是否是托管服务 1 是 0 否")
     private Integer isBoarding;
@@ -30,59 +31,4 @@ public class Service {
     @ApiModelProperty(value = "服务时间")
     private String serviceTime;
 
-    public Integer getSid() {
-        return sid;
-    }
-
-    public void setSid(Integer sid) {
-        this.sid = sid;
-    }
-
-    public Integer getServiceProviderId() {
-        return serviceProviderId;
-    }
-
-    public void setServiceProviderId(Integer serviceProviderId) {
-        this.serviceProviderId = serviceProviderId;
-    }
-
-    public String getServiceName() {
-        return serviceName;
-    }
-
-    public void setServiceName(String serviceName) {
-        this.serviceName = serviceName == null ? null : serviceName.trim();
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Integer getIsBoarding() {
-        return isBoarding;
-    }
-
-    public void setIsBoarding(Integer isBoarding) {
-        this.isBoarding = isBoarding;
-    }
-
-    public Date getServiceDate() {
-        return serviceDate;
-    }
-
-    public void setServiceDate(Date serviceDate) {
-        this.serviceDate = serviceDate;
-    }
-
-    public String getServiceTime() {
-        return serviceTime;
-    }
-
-    public void setServiceTime(String serviceTime) {
-        this.serviceTime = serviceTime == null ? null : serviceTime.trim();
-    }
 }
